d25575c2ac9d7db84a56a47b560b4d59ec356227
Isaque Cardoso <77692909+isaquecardoso@users.noreply.github.com>
2021-10-19 14:00:11 -0300

    Add files via upload

---
+++ assets/js/slider.js
@@ -0,0 +1,39 @@
+﻿let slidePostion = 0;
+const slides = document.getElementsByClassName('carousel_item');
+const totalSlides = slides.length;
+
+document
+    .getElementById('carousel-next')
+    .addEventListener("click", function(){
+        moveToNextSlide()
+    });
+document
+    .getElementById('carousel-prev')
+    .addEventListener("click", function(){
+        moveToPrevSlide()
+    });
+
+function updateSlidePosition(){
+    for (let slide of slides){
+        slide.classList.remove('carousel_active');
+        slide.classList.add('carousel_hidden');
+    }
+    slides[slidePostion].classList.add('carousel_active');
+}
+
+function moveToNextSlide(){
+    if (slidePostion == totalSlides - 1){
+        slidePostion = 0;
+    } else{
+        slidePostion++;
+    }
+    updateSlidePosition()
+}
+function moveToPrevSlide(){
+    if (slidePostion == 0){
+        slidePostion == totalSlides - 1;
+    } else{
+        slidePostion--;
+    }
+    updateSlidePosition();
+}
